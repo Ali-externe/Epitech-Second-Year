586e580ac32fe731ae3906f9566849395c821f73
Yaich Ali <66366945+Ali-externe@users.noreply.github.com>
2021-09-04 02:03:58 +0200

    Add files via upload

---
+++ OOP_indie_studio_2018/CMakeFiles/bomberman.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/foulilou/tek2/OOP_indie_studio_2018/src/Bonus.cpp" "/home/foulilou/tek2/OOP_indie_studio_2018/CMakeFiles/bomberman.dir/src/Bonus.cpp.o"
+  "/home/foulilou/tek2/OOP_indie_studio_2018/src/Ia.cpp" "/home/foulilou/tek2/OOP_indie_studio_2018/CMakeFiles/bomberman.dir/src/Ia.cpp.o"
+  "/home/foulilou/tek2/OOP_indie_studio_2018/src/bomb.cpp" "/home/foulilou/tek2/OOP_indie_studio_2018/CMakeFiles/bomberman.dir/src/bomb.cpp.o"
+  "/home/foulilou/tek2/OOP_indie_studio_2018/src/getMapCoor.cpp" "/home/foulilou/tek2/OOP_indie_studio_2018/CMakeFiles/bomberman.dir/src/getMapCoor.cpp.o"
+  "/home/foulilou/tek2/OOP_indie_studio_2018/src/main.cpp" "/home/foulilou/tek2/OOP_indie_studio_2018/CMakeFiles/bomberman.dir/src/main.cpp.o"
+  "/home/foulilou/tek2/OOP_indie_studio_2018/src/map.cpp" "/home/foulilou/tek2/OOP_indie_studio_2018/CMakeFiles/bomberman.dir/src/map.cpp.o"
+  "/home/foulilou/tek2/OOP_indie_studio_2018/src/menu.cpp" "/home/foulilou/tek2/OOP_indie_studio_2018/CMakeFiles/bomberman.dir/src/menu.cpp.o"
+  "/home/foulilou/tek2/OOP_indie_studio_2018/src/player.cpp" "/home/foulilou/tek2/OOP_indie_studio_2018/CMakeFiles/bomberman.dir/src/player.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "./include"
+  "./src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ OOP_indie_studio_2018/CMakeFiles/bomberman.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bomberman.dir/src/Bonus.cpp.o"
+  "CMakeFiles/bomberman.dir/src/Ia.cpp.o"
+  "CMakeFiles/bomberman.dir/src/bomb.cpp.o"
+  "CMakeFiles/bomberman.dir/src/getMapCoor.cpp.o"
+  "CMakeFiles/bomberman.dir/src/main.cpp.o"
+  "CMakeFiles/bomberman.dir/src/map.cpp.o"
+  "CMakeFiles/bomberman.dir/src/menu.cpp.o"
+  "CMakeFiles/bomberman.dir/src/player.cpp.o"
+  "bomberman"
+  "bomberman.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bomberman.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
